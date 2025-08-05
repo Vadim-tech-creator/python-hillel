80b0234c6b65d9b41bf9e9fe46a4a6919be737b8
vadimvadim <vadikbond8190@gmail.com>
2025-08-05 16:18:25 +0200

    first commit

---
+++ lesson7/homework7.3.py
@@ -0,0 +1,13 @@
+def second_index(text, some_str):
+    first_pos = text.find(some_str)
+    if first_pos == -1:
+        return None
+    second_pos = text.find(some_str, first_pos + 1)
+    if second_pos == -1:
+        return None
+    return second_pos
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
---
+++ lesson7/homework7.4.py
@@ -0,0 +1,8 @@
+def common_elements():
+    multiples_of_3 = (x for x in range(100) if x % 3 == 0)
+    multiples_of_5 = (x for x in range(100) if x % 5 == 0)
+    set_3 = set(multiples_of_3)
+    set_5 = set(multiples_of_5)
+
+    return set_3 & set_5
+assert common_elements() == {0, 15, 30, 45, 60, 75, 90}
